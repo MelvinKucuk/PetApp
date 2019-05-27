0d443d03cd37afbd7ecf881c615d92dacebc815c
Unknown <melvinalek@gmail.com>
2019-05-27 11:43:41 -0300

    Fix de RequestPermission

--- app/src/main/java/com/melvin/flux/view/MapFragment.java
+++ app/src/main/java/com/melvin/flux/view/MapFragment.java
@@ -10,6 +10,7 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
@@ -33,6 +34,8 @@ public class MapFragment extends Fragment implements LocationListener {
     private Location location;
     private double latitude;
     private double longitude;
+    private GoogleMap googleMap;
+    private LocationManager locationManager;
 
     public static final int REQUEST_LOCATION = 4;
 
@@ -51,62 +54,64 @@ public class MapFragment extends Fragment implements LocationListener {
         mapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(final GoogleMap mMap) {
-                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                googleMap = mMap;
+                googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
-                mMap.clear();
+                googleMap.clear();
 
-                LocationManager locationManager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
+                locationManager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
                 if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                         && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                     requestPermissions(new String[]{
                             Manifest.permission.ACCESS_COARSE_LOCATION,
                             Manifest.permission.ACCESS_FINE_LOCATION
                     }, REQUEST_LOCATION);
-                }
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 120000, 1, new LocationListener() {
-                    @Override
-                    public void onLocationChanged(Location location) {
+                }else {
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 120000, 1, new LocationListener() {
+                        @Override
+                        public void onLocationChanged(Location location) {
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
+
+                            CameraPosition googlePlex = CameraPosition.builder()
+                                    .target(new LatLng(latitude, longitude))
+                                    .zoom(10)
+                                    .bearing(0)
+                                    .build();
+
+                            googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(googlePlex), 10000, null);
+
+                            googleMap.addMarker(new MarkerOptions()
+                                    .position(new LatLng(latitude, longitude))
+                                    .icon(bitmapDescriptorFromVector(getActivity(), R.drawable.ic_location_24dp)));
+                        }
+
+                        @Override
+                        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+                        }
+
+                        @Override
+                        public void onProviderEnabled(String s) {
+
+                        }
+
+                        @Override
+                        public void onProviderDisabled(String s) {
+
+                        }
+                    });
+                    if (location != null) {
+                        location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
                         latitude = location.getLatitude();
                         longitude = location.getLongitude();
-
-                        CameraPosition googlePlex = CameraPosition.builder()
-                                .target(new LatLng(latitude, longitude))
-                                .zoom(10)
-                                .bearing(0)
-                                .build();
-
-                        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(googlePlex), 10000, null);
-
-                        mMap.addMarker(new MarkerOptions()
-                                .position(new LatLng(latitude, longitude))
-                                .icon(bitmapDescriptorFromVector(getActivity(), R.drawable.ic_location_24dp)));
                     }
-
-                    @Override
-                    public void onStatusChanged(String s, int i, Bundle bundle) {
-
-                    }
-
-                    @Override
-                    public void onProviderEnabled(String s) {
-
-                    }
-
-                    @Override
-                    public void onProviderDisabled(String s) {
-
-                    }
-                });
-                if (location != null) {
-                    location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                    latitude = location.getLatitude();
-                    longitude = location.getLongitude();
                 }
 
-                mMap.addMarker(new MarkerOptions()
+                googleMap.addMarker(new MarkerOptions()
                         .position(new LatLng(-34.9208142, -57.9518059)));
 
-                mMap.addMarker(new MarkerOptions()
+                googleMap.addMarker(new MarkerOptions()
                         .position(new LatLng(-34.6188126, -58.3677217)));
             }
         });
@@ -142,5 +147,61 @@ public class MapFragment extends Fragment implements LocationListener {
     @Override
     public void onProviderDisabled(String s) {
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case REQUEST_LOCATION: {
+
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                            && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 120000, 1, new LocationListener() {
+                            @Override
+                            public void onLocationChanged(Location location) {
+                                latitude = location.getLatitude();
+                                longitude = location.getLongitude();
+
+                                CameraPosition googlePlex = CameraPosition.builder()
+                                        .target(new LatLng(latitude, longitude))
+                                        .zoom(10)
+                                        .bearing(0)
+                                        .build();
+
+                                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(googlePlex), 10000, null);
+
+                                googleMap.addMarker(new MarkerOptions()
+                                        .position(new LatLng(latitude, longitude))
+                                        .icon(bitmapDescriptorFromVector(getActivity(), R.drawable.ic_location_24dp)));
+
+                            }
+
+                            @Override
+                            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+                            }
+
+                            @Override
+                            public void onProviderEnabled(String s) {
+
+                            }
+
+                            @Override
+                            public void onProviderDisabled(String s) {
+
+                            }
+                        });
+                        if (location != null) {
+                            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
+                        }
+                    }
+                }
+
+                return;
+            }
+        }
     }
 }
